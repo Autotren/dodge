e44a9a118a5b9b5be8933122e90fb85eae3824a4
PabloHernandez0 <pablusky2004@hotmail.com>
2020-10-30 19:47:36 +0100

    Enemigos

--- Player.gd
+++ Player.gd
@@ -43,6 +43,7 @@ func _on_Player_body_entered(body):
 	hide()  # Player disappears after being hit.
 	emit_signal("hit")
 	$CollisionShape2D.set_deferred("disabled", true)
+
 func start(pos):
 	position = pos
 	show()
