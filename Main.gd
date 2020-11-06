ba04e025cb15b420012f11ecc2d3e99a7c937914
pathMarker <pathmarkerer@gmail.com>
2020-11-06 19:38:34 +0100

    Final

--- Main.gd
+++ Main.gd
@@ -15,12 +15,20 @@ func _ready():
 
 
 func game_over():
-	pass # Replace with function body.
+	$ScoreTimer.stop()
+	$MobTimer.stop()
+	$HUD.show_game_over()
+	get_tree().call_group("mobs", "queue_free")
+	$Music.stop()
+	$DeathSound.play()
 
 func new_game():
 	score = 0
 	$Player.start($StartPosition.position)
 	$StartTimer.start()
+	$HUD.update_score(score)
+	$HUD.show_message("Get Ready")
+	$Music.play()
 
 func _on_StartTimer_timeout():
 	$MobTimer.start()
@@ -28,6 +36,7 @@ func _on_StartTimer_timeout():
 
 func _on_ScoreTimer_timeout():
 	score += 1
+	$HUD.update_score(score)
 
 func _on_MobTimer_timeout():
 	# Choose a random location on Path2D.
